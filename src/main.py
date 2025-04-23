14b9594663c00d9e8325b2981c19c147b8afb6ba
Ned Olsen <71749344+theneddlesking@users.noreply.github.com>
2025-04-23 11:14:21 +1000

    Clean up main.py

--- src/main.py
+++ src/main.py
@@ -2,7 +2,6 @@
 
 from app import App
 from mas.examples.mas_testing import test_basic_mas
-from mas.examples.task_testing import test_task
 
 
 def main():
@@ -10,9 +9,7 @@ def main():
     app = App()
     app.run()
 
-    test_task(app)
-    # test_yaml_to_pydantic(app)
-    # test_basic_mas(app)
+    test_basic_mas(app)
 
 
 if __name__ == "__main__":
