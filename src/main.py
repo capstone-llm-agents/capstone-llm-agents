2b211a65b1149c5007596cfb17fd969d2a6e1a42
Adrien Krebet <122225640+adrien-krebet@users.noreply.github.com>
2025-05-06 14:51:05 +1000

    Revert "Recipe agent and data"

--- src/main.py
+++ src/main.py
@@ -1,15 +1,15 @@
 """Entry point of the program."""
 
 from app import App
-#from mas.examples.mas_testing import test_basic_mas
-from mas.examples.recipe_testing import test_recipe_mas
+from mas.examples.mas_testing import test_basic_mas
+
 
 def main():
     """Entry point of the program."""
     app = App()
     app.run()
 
-    test_recipe_mas(app)
+    test_basic_mas(app)
 
 
 if __name__ == "__main__":
