73a31ee82adccad5edfd243f4ee8cd8a5329fe0e
Ned Olsen <71749344+theneddlesking@users.noreply.github.com>
2025-05-13 10:03:08 +1000

    Fill out MAS and agent functions

--- src/core/agent.py
+++ src/core/agent.py
@@ -26,10 +26,55 @@ class Agent(Entity):
     def handle_query(self, query: Query) -> QueryResponse:
         """Handle a query by executing it and returning the result."""
 
-        # TODO use the capabilities to handle the query
+        # build prompt from the query
+        original_query = query.content
 
-        # NOTE: this should be able to be implemented now, but still working on it
-        raise NotImplementedError("handle_query not implemented")
+        # relevant memories
+        relevant_memories = self.capabilties.memory.load_memories_relevant_to_query(
+            query.content
+        )
+
+        memory_str = "\n".join([f" - {memory.content}" for memory in relevant_memories])
+
+        # relevant knowledge
+        relevant_knowledge = self.capabilties.knowledge_base.retrieve_related_knowledge(
+            query.content
+        )
+
+        knowledge_str = "\n".join(
+            [f" - {knowledge.knowledge}" for knowledge in relevant_knowledge]
+        )
+
+        # prompt inject
+        prompt = f"""
+        Memories:
+        {memory_str}
+        Knowledge:
+        {knowledge_str}
+        Query:
+        {original_query}
+        Answer:
+        """
+
+        new_query = Query(query.who, prompt)
+
+        # check if the query needs planning
+        if self.capabilties.planning.query_needs_planning(new_query.content):
+            # create a plan
+            plan = self.capabilties.planning.create_plan(new_query.content)
+
+            # build an executor for the plan
+            executor = self.capabilties.planning.build_executor(new_query.content, plan)
+
+            # execute the plan
+            result = executor.answer_query(original_query)
+
+            # return the result
+            return QueryResponse(self, result)
+
+        result = self.capabilties.query_executor.answer_query(new_query.content)
+
+        return QueryResponse(self, result)
 
     def get_capabilities(self) -> AgentCapabilities:
         """Get the capabilities of the agent."""
--- src/core/mas.py
+++ src/core/mas.py
@@ -1,7 +1,17 @@
+from enum import Enum
 from core.agent import Agent
 from core.chat import ChatHistory, ChatMessage, Query
 from core.entity import HumanUser
 from core.communication_protocol import CommunicationProtocol
+
+
+class MASResponse(Enum):
+    """An enum representing the response of the MAS."""
+
+    WAITING_FOR_USER_CONFIRMATION = "waiting_for_user_confirmation"
+    AUTO_RUN_TIMEOUT = "auto_run_timeout"
+    WAITING_FOR_USER_QUERY = "waiting_for_user_query"
+    WAITING_FOR_USER_RESPONSE = "waiting_for_user_response"
 
 
 class MAS:
@@ -24,6 +34,9 @@ class MAS:
         self.max_auto_run_steps = 10
 
         self.step_count = 0
+
+        # query to ask the user
+        self.query_to_ask_user: Query | None = None
 
     def reset_steps(self):
         """Reset the step count."""
@@ -65,8 +78,11 @@ class MAS:
         # run the next step, executing the query
         self.run_next_step()
 
-    def run_next_step(self):
+    def run_next_step(self) -> MASResponse:
         """Step through the MAS."""
+
+        # clear the query to ask user
+        self.clear_query_to_ask_user()
 
         # increment the step count if auto running
         if self.auto_run:
@@ -75,8 +91,7 @@ class MAS:
         # check auto run max steps
         if self.auto_run and self.step_count >= self.max_auto_run_steps:
             # if we are auto running, we need to stop
-            self.handle_auto_run_timeout()
-            return
+            return self.handle_auto_run_timeout()
 
         # create a query
         query = self.communication_protocol.create_query(self.chat_history)
@@ -84,13 +99,13 @@ class MAS:
         # check if it needs human input (e.g. can't automatically run)
         if query.who == self.user:
             # if it is the user, we need to wait for input
-            self.ask_user_query(query)
-            return
+            self.set_query_to_ask_user(query)
+            return self.ask_user_query()
 
         # if we are not auto running, we need to wait for input to confirm
         if not self.auto_run:
             # ask the user to confirm to continue
-            self.wait_for_user_confirmation(query)
+            return self.wait_for_user_confirmation()
 
         # get agent from query
         agent = self.get_agent(query.who.name)
@@ -101,14 +116,40 @@ class MAS:
         # add the response to the chat history
         self.chat_history.add_message(response)
 
-    def ask_user_query(self, query: Query):
+        # wait for next user query
+        return self.wait_for_user_query()
+
+    def resume_from_user_confirmation(self, query: Query):
+        """Resume the MAS from user confirmation."""
+        # get agent from query
+        agent = self.get_agent(query.who.name)
+
+        # ask the agent
+        response = agent.handle_query(query)
+
+        # add the response to the chat history
+        self.chat_history.add_message(response)
+
+    def set_query_to_ask_user(self, query: Query):
+        """Set the query to ask the user."""
+        self.query_to_ask_user = query
+
+    def clear_query_to_ask_user(self):
+        """Clear the query to ask the user."""
+        self.query_to_ask_user = None
+
+    def ask_user_query(self) -> MASResponse:
         """Ask the user a query."""
-        raise NotImplementedError("ask_user_query not implemented")
+        return MASResponse.WAITING_FOR_USER_RESPONSE
 
-    def handle_auto_run_timeout(self):
+    def handle_auto_run_timeout(self) -> MASResponse:
         """Handle the auto run timeout."""
-        raise NotImplementedError("handle_auto_run_timeout not implemented")
+        return MASResponse.AUTO_RUN_TIMEOUT
 
-    def wait_for_user_confirmation(self, query: Query):
+    def wait_for_user_confirmation(self) -> MASResponse:
         """Wait for user confirmation to continue."""
-        raise NotImplementedError("wait_for_user_confirmation not implemented")
+        return MASResponse.WAITING_FOR_USER_CONFIRMATION
+
+    def wait_for_user_query(self) -> MASResponse:
+        """Wait for the user to ask a query."""
+        return MASResponse.WAITING_FOR_USER_QUERY
