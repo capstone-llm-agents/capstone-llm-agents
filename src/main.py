bdb6a8988661086d354c6ec0d585d18659e46d1b
Ned Olsen <71749344+theneddlesking@users.noreply.github.com>
2025-05-13 11:17:10 +1000

    Update main.py

--- src/main.py
+++ src/main.py
@@ -4,6 +4,7 @@ from app import App
 from core.api import MASAPI
 from core.capabiliity_manager import AgentCapabilities
 from core.capabiliity_proxy import CapabilityProxy
+from core.capability import Capability
 from core.entity import HumanUser
 from core.mas import MAS
 from models.ag2_model import AG2Model
@@ -12,7 +13,9 @@ from spoof.spoofed_comm_protocol import CommunicationProtocolSpoof
 from user_interface.inteface import UserInterface
 
 
-def generate_capabilities_for_ag2_agent(agent: ConversableAgent) -> AgentCapabilities:
+def generate_capabilities_for_ag2_agent(
+    agent: ConversableAgent, agent_capabilities: list[Capability]
+) -> AgentCapabilities:
     """Generate capabilities for the AG2 agent."""
     # agent capabiltiies using proxy
     supported_extensions: list[str] = []
@@ -23,11 +26,18 @@ def generate_capabilities_for_ag2_agent(agent: ConversableAgent) -> AgentCapabil
 
     proxy = CapabilityProxy(spoofed_capabilities)
 
+    for capability in agent_capabilities:
+        proxy.add_capability(capability)
+
     return proxy.build_capabilities_manager()
 
 
 user = HumanUser("User", "The human user of the MAS")
 communication_protocol = CommunicationProtocolSpoof(user)
+
+# NOTE: Here you can add the capabilities that do not need to be spoofed
+# (completed capabilities ready to use)
+capabilities: list[Capability] = []
 
 mas = MAS(communication_protocol, user)
 api = MASAPI(mas)
