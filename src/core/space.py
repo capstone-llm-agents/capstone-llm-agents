36f51341d5ad57697e89b0cb638b29cf0e178750
Ned Olsen <71749344+theneddlesking@users.noreply.github.com>
2025-06-03 20:29:00 +1000

    Update space.py

--- src/core/space.py
+++ src/core/space.py
@@ -1,3 +1,4 @@
+from core.agent import Agent
 from core.chat import ChatHistory
 from core.entity import Entity
 from core.mas import MAS
@@ -6,10 +7,12 @@ from core.mas import MAS
 class Space:
     """A space holds some entities and a chat history between them."""
 
+    name: str
     entities: list[Entity]
     chat_history: ChatHistory
 
-    def __init__(self, mas: MAS):
+    def __init__(self, name: str, mas: MAS):
+        self.name = name
         self.entities = []
         self.chat_history = ChatHistory()
         self.mas = mas
@@ -20,6 +23,24 @@ class Space:
 
     def get_chat_history(self) -> ChatHistory:
         """Get the chat history in the space."""
+        return self.chat_history
 
-        # TODO remove this spoof and get the chat history from the space not MAS
-        return self.mas.chat_history
+    def add_entity(self, entity: Entity):
+        """Add an entity to the space."""
+        if entity not in self.entities:
+            self.entities.append(entity)
+
+
+class MASSpace(Space):
+    """A space that is part of a Multi-Agent System (MAS)."""
+
+    assistant: Agent
+    """The assistant agent talks to the user."""
+
+    discovery_agent: Agent
+    """The discovery agent helps the assistant find other agents outside the MAS."""
+
+    def __init__(self, name: str, mas: MAS, assistant: Agent, discovery_agent: Agent):
+        super().__init__(name, mas)
+        self.assistant = assistant
+        self.discovery_agent = discovery_agent
