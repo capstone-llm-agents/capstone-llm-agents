dfea4955da5965be820630f76d4418481df0b0fd
101100545 <37169494+101100545@users.noreply.github.com>
2025-08-20 03:00:13 +1000

    CAP-99 Added document creator function/tool to MCP server

--- mcp_server/test_server.py
+++ mcp_server/test_server.py
@@ -8,6 +8,11 @@ from starlette.applications import Starlette
 from starlette.requests import Request
 from starlette.responses import Response
 from starlette.routing import Mount, Route
+from datetime import datetime
+from xhtml2pdf import pisa
+import os
+import io
+
 
 # create the MCP server
 mcp = FastMCP("SSE Example Server")
@@ -24,6 +29,67 @@ def add(a: int, b: int) -> str:
     """Add two numbers and return the result."""
     return f"The sum of {a} and {b} is {a + b}."
 
+@mcp.tool()
+def document_creator(content: str, base_filename: str ="report") -> str:
+    """
+    Create a PDF travel plan document based on the template provided
+    Template Structure:
+    <!DOCTYPE html>
+    <html>
+    <head>
+    <meta charset="UTF-8">
+    <title>Agent Report</title>
+    <style>
+      body { font-family: serif; }
+      h1 { color: green; }
+      p { font-size: 12pt; margin-bottom: 5px;}
+      h2, h3 { color: navy; margin-top: 20px; margin-bottom: 10px;}
+      ul, ol { margin-bottom: 10px;}
+      strong { font-weight: bold; }
+    </style>
+    </head>
+    <body>
+        <h2>[Fill Title Here]</h2>
+        <ol>
+            [Create list items <li> for available dates here]
+        </ol>
+        <h3>Travel Details:</h3>
+        <p><strong>Start Location:</strong> [Fill Start Location Here]</p>
+        <p><strong>Destination: </strong> [Fill Destination Here]</p>
+        <p><strong>Time Taken: </strong> [Fill Time Taken Here]</p>
+        <p><strong>Transport Mode: </strong> [Fill Transport Mode Here]</p>
+        <p><strong>Date&Time: </strong> [Fill Date & Time Here]</p>
+    </body>
+    </html>
+    """
+    print("📄 DOCUMENT_CREATOR TOOL CALLED")
+    print(content[:100])  # Preview
+    main_content = content
+    print("being called")
+    script_path = os.path.abspath(__file__)
+
+    script_directory = os.path.dirname(script_path)
+    print("script directory")
+
+    timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
+    output_filename = f"{base_filename}_{timestamp}.pdf"
+    print("filename")
+    output_filepath = os.path.join(script_directory, output_filename)
+    print("filepath")
+
+    source_html = io.BytesIO(content.encode("utf-8"))
+    print("sourcehtml")
+    with open(output_filepath, "w+b") as output_pdf:
+            pisa_status = pisa.CreatePDF(
+                source_html,
+                dest=output_pdf
+            )
+    print ("open pdf")        
+
+    if not pisa_status.err:
+        return 'Document Created'
+    else:
+        return 'Document not created'
 
 # example resource
 @mcp.resource(uri="resource://hello")
