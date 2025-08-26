c1a78dc451c0600e2850e2a01f27f9fa4dd8e572
Ned Olsen <71749344+theneddlesking@users.noreply.github.com>
2025-08-27 09:17:54 +1000

    Add friends

--- llm_mas/mas/entity.py
+++ llm_mas/mas/entity.py
@@ -1,5 +1,7 @@
 """An entity is a User or an Agent in the MAS."""
 
+from __future__ import annotations
+
 
 class Entity:
     """Base class for entities in the MAS."""
@@ -9,6 +11,17 @@ class Entity:
         self.name = name
         self.role = role
 
+        # friends
+        self.friends: list[Entity] = []
+
     def get_name(self) -> str:
         """Return the name of the entity."""
         return self.name
+
+    def get_role(self) -> str:
+        """Return the role of the entity."""
+        return self.role
+
+    def add_friend(self, friend: Entity) -> None:
+        """Add a friend to the entity."""
+        self.friends.append(friend)
