adf093d7ff107bb353ee2e3bde933361fe7ab00b
atom300 <149028972+atom300@users.noreply.github.com>
2025-09-23 16:26:17 +1000

    Update call_llm.py
    update the chat completion to use Async

--- llm_mas/model_providers/openai/call_llm.py
+++ llm_mas/model_providers/openai/call_llm.py
@@ -1,12 +1,12 @@
 import os
 import asyncio
-from openai import OpenAI
+from openai import AsyncOpenAI
 from llm_mas.mas.conversation import UserAssistantExample, UserMessage
 
 async def call_llm(prompt: str) -> str:
-    client = OpenAI(api_key=os.environ.get("OPENAI_API_KEY"))
+    client = AsyncOpenAI(api_key=os.environ.get("OPENAI_API_KEY"))
     model = "gpt-4o-mini"
-    response = client.chat.completions.create(
+    response = await client.chat.completions.create(
         model=model,
         messages=[{"role": "user", "content": prompt}],
     )
@@ -18,9 +18,9 @@ async def call_llm(prompt: str) -> str:
 
 
 async def call_llm_with_messages(messages: list[dict]) -> str:
-    client = OpenAI(api_key=os.environ.get("OPENAI_API_KEY"))
+    client = AsyncOpenAI(api_key=os.environ.get("OPENAI_API_KEY"))
     model = "gpt-4o-mini"
-    response = client.chat.completions.create(
+    response = await client.chat.completions.create(
         model=model,
         messages=messages,
     )
@@ -37,9 +37,9 @@ async def call_llm_with_examples(
 ) -> str:
     messages = [example.user_message.as_dict() for example in examples]
     messages.append(user_message.as_dict())
-    client = OpenAI(api_key=os.environ.get("OPENAI_API_KEY"))
+    client =  AsyncOpenAI(api_key=os.environ.get("OPENAI_API_KEY"))
     model = "gpt-4o-mini"
-    response = client.chat.completions.create(
+    response = await client.chat.completions.create(
         model=model,
         messages=messages,
     )
@@ -48,16 +48,15 @@ async def call_llm_with_examples(
         msg = f"No content returned from {model} with examples."
         raise ValueError(msg)
     return content
-    
+
 async def get_embedding(text: str) -> list[float]:
-    """Get the embedding for the given text using Ollama."""
+    """Get the embedding for the given text using openai."""
     model = "text-embedding-3-small"
-    client = OpenAI(api_key=os.environ.get("OPENAI_API_KEY"))
-    response = client.embeddings.create(
+    client =  AsyncOpenAI(api_key=os.environ.get("OPENAI_API_KEY"))
+    response = await client.embeddings.create(
         model=model,
         input=text,
     )
-
     # as vector
     embeddings = response.data[0].embedding
     if not embeddings or len(embeddings) == 0:
