06b1c36bca1b4448218df0e57af6db435f013812
atom300 <a.safdari23@pm.me>
2025-10-21 20:18:11 +1100

    uses the long term memory save action rather then saving the memory inside the stop action.

--- llm_mas/action_system/base/actions/stop.py
+++ llm_mas/action_system/base/actions/stop.py
@@ -2,9 +2,9 @@
 
 import datetime
 from typing import override
-
+import asyncio
 from mem0 import Memory as Mem
-
+from components.actions.memory import MemorySaveLong
 from llm_mas.action_system.core.action import Action
 from llm_mas.action_system.core.action_context import ActionContext
 from llm_mas.action_system.core.action_params import ActionParams
@@ -26,33 +26,8 @@ class StopAction(Action):
         if not GENERAL_CONFIG.app.memory_enabled():
             return ActionResult()
 
-        config = {
-            "vector_store": {
-                "provider": "chroma",
-                "config": {
-                    "collection_name": "test",
-                    "path": "db",
-                },
-            },
-        }
-        m = Mem.from_config(config)
-        chat_history = context.conversation.get_chat_history()
-        messages = chat_history.as_dicts()
-        last_message = messages[-1]
-        memory_to_save_user = f"User said {last_message['content']}"
-        second_last_message = messages[-2]
-        memory_to_save_agent = f"Agent said {second_last_message['content']}"
-        now = datetime.datetime.now(tz=datetime.UTC)
 
-        date_string = now.strftime("%Y-%m-%d %H:%M:%S")
-        m.add(
-            messages=memory_to_save_user,
-            agent_id=context.agent.name,
-            metadata={"Speaker": "User", "timestamp": date_string},
-        )
-        m.add(
-            messages=memory_to_save_agent,
-            agent_id=context.agent.name,
-            metadata={"speaker": "Agent", "timestamp": date_string},
-        )
+        memory = MemorySaveLong()
+        await memory.do(params, context)
+
         return ActionResult()
