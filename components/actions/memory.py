f78995bc906d81287e946aab9da746c1838d4a91
atom300 <a.safdari23@pm.me>
2025-10-30 18:48:19 +1100

    updated the memory action to return no memories on windows as i can't figure out why it keeps crashing on windows because of the memory

--- components/actions/memory.py
+++ components/actions/memory.py
@@ -1,4 +1,3 @@
-import asyncio
 from datetime import datetime
 from typing import override
 from mem0 import AsyncMemory
@@ -8,7 +7,7 @@ from llm_mas.action_system.core.action_params import ActionParams
 from llm_mas.action_system.core.action_result import ActionResult
 from llm_mas.logging.loggers import APP_LOGGER
 from llm_mas.utils.memory_config import InternalMemoryConfig as MemoryConfig
-
+import sys
 
 
 
@@ -58,17 +57,21 @@ async def Search(context: ActionContext, config):
     """helper function from previous threading tried to move it back since its async, and it broke
         Save the memory to the memory store."""
     APP_LOGGER.info("Searching memory")
-    memory = AsyncMemory(config=config)
-    chat_history = context.conversation.get_chat_history()
+    if sys.platform == "win32":
+        memories_str = "No memories found"
+        return memories_str
+    else:
+        memory = AsyncMemory(config=config)
+        chat_history = context.conversation.get_chat_history()
 
-    messages = chat_history.as_dicts()
+        messages = chat_history.as_dicts()
 
-    last_message = messages[-1]
+        last_message = messages[-1]
 
-    relevant_memories = await memory.search(query=last_message["content"], agent_id=context.agent.name, limit=10)
-    # relevant_memories = m.search(query=last_message["content"], limit=10)
-    memories_str = "\n".join(f"- {entry['memory']}" for entry in relevant_memories["results"])
-    return memories_str
+        relevant_memories = await memory.search(query=last_message["content"], agent_id=context.agent.name, limit=10)
+        # relevant_memories = m.search(query=last_message["content"], limit=10)
+        memories_str = "\n".join(f"- {entry['memory']}" for entry in relevant_memories["results"])
+        return memories_str
 
 
 class MemorySaveLong(Action):
